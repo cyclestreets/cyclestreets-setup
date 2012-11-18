1094fbcbd4b6911863cf3cedea0363f9332f52da
Martin Lucas-Smith <martin@lucas-smith.co.uk>
2012-11-18 13:28:15 +0000

    Implemented a new test (retrieve existing journey)

--- sms-monitoring/run.php
+++ sms-monitoring/run.php
@@ -112,18 +112,18 @@ class doCheck
 	
 	
 	# Route-planning check
-	private function test_journey (&$errorMessage = false, &$result = false)
+	private function test_journey_new (&$errorMessage = false, &$result = false)
 	{
 		# Plan a route (the split is to avoid bots traversing a repository)
 		$routeUrl = "http://www.cyclestreets.net" . "/api/journey.json?key={$this->cyclestreetsApiKey}&plan=quietest&itinerarypoints=-0.140085,51.502022,Buckingham+Palace|-0.129204,51.504353,Horse+Guards+Parade|-0.129394,51.499496,Westminster+Abbey";
 		if (!$json = file_get_contents ($routeUrl)) {
-			$errorMessage = "Could not retrieve results of /api/journey call within {$this->timeoutSeconds} seconds.";
+			$errorMessage = "Could not retrieve results of /api/journey call (new journey) within {$this->timeoutSeconds} seconds.";
 			return false;
 		}
 		
 		# Decode the JSON
 		$result = json_decode ($json, true);
-		// var_dump ($result);
+		// print_r ($result);
 		// file_put_contents ('./results.txt', print_r ($result, 1));
 		
 		# Ensure the data is as expected
@@ -147,15 +147,63 @@ class doCheck
 			|| ($result['waypoint'][0]['@attributes']['longitude'] != '-0.140085')
 			
 			# Testing..
-			// || !isSet ($result['markerx'])
+			// || !isSet ($result['doesnotexist'])
 		) {
-			$errorMessage = "The /api/journey call did not return the expected journey format.";
+			$errorMessage = "The /api/journey call (new journey) did not return the expected format.";
 			return false;
 		}
 		
 		# Return success
 		return true;
 	}
+	
+	
+	# Route-retrieval check
+	private function test_journey_existing (&$errorMessage = false, &$result = false)
+	{
+		# Plan a route (the split is to avoid bots traversing a repository)
+		$routeUrl = "http://www.cyclestreets.net" . "/api/journey.json?key={$this->cyclestreetsApiKey}&plan=fastest&itinerary=345529";
+		if (!$json = file_get_contents ($routeUrl)) {
+			$errorMessage = "Could not retrieve results of /api/journey call (retrieve journey) within {$this->timeoutSeconds} seconds.";
+			return false;
+		}
+		
+		# Decode the JSON
+		$result = json_decode ($json, true);
+		// print_r ($result);
+		// file_put_contents ('./results.txt', print_r ($result, 1));
+		
+		# Ensure the data is as expected
+		if (
+			# Check the marker structure has the first marker
+			   !isSet ($result['marker'])
+			|| !isSet ($result['marker'][0])
+			|| !isSet ($result['marker'][0]['@attributes'])
+			|| !isSet ($result['marker'][0]['@attributes']['start'])
+			|| ($result['marker'][0]['@attributes']['finish'] != 'Thoday Street')
+			
+			# Check for a co-ordinate in the right area of the country
+			|| !isSet ($result['marker'][0]['@attributes']['coordinates'])
+			|| (!substr_count ($result['marker'][0]['@attributes']['coordinates'], '0.117867,52.205288 0.117872,52.205441'))
+			
+			# Check for a valid waypoint
+			|| !isSet ($result['waypoint'])
+			|| !isSet ($result['waypoint'][0])
+			|| !isSet ($result['waypoint'][0]['@attributes'])
+			|| !isSet ($result['waypoint'][0]['@attributes']['longitude'])
+			|| ($result['waypoint'][0]['@attributes']['longitude'] != '0.117950')
+			
+			# Testing..
+			// || !isSet ($result['doesnotexist'])
+		) {
+			$errorMessage = "The /api/journey call (retrieve journey) did not return the expected format.";
+			return false;
+		}
+		
+		# Return success
+		return true;
+	}
+	
 	
 }
 

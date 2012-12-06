cedc9d2022f39471d2821628be5f66325e7b683b
mvl22 <martin@lucas-smith.co.uk>
2012-12-06 22:19:59 +0000

    Retrieve a importdefinition.ini file which specifies the parameters of the import it has generated; the ini file also takes responsibility for the acceptable download time period

--- install-routing-data/run.sh
+++ install-routing-data/run.sh
@@ -52,13 +52,48 @@ fi
 
 ## Attempt to get the latest import
 
-# Only allow this script to run between the specified times as the download can be large and disrupt main site performance.
-hour=$(date +%H)
-if [ $hour -gt $importStartHourLast -o $hour -lt $importStartHourFirst ]
-then
-	echo "# The import can only be downloaded between the times specified, to avoid disrupting main site performance; alternatively change the values in the config file"
-	exit
+# Ensure import machine and definition file variables has been defined
+if [ -z "${importMachineAddress}" -o -z "${importMachineFile}" ]; then
+	echo "# An import machine and definition file must be defined in order to run an import"
+	exit 1
 fi
+
+# Retrieve the routing definition file from the import machine
+set +e
+sudo -u $username scp ${username}@${importMachineAddress}:${importMachineFile} ${websitesBackupsFolder} >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "# The import machine file could not be retrieved; please check the 'importMachineAddress' and 'importMachineFile' settings"
+	exit 1
+fi
+set -e
+
+# Get the required variables from the routing definition file; this is not directly executed for security
+# Sed extraction method as at http://stackoverflow.com/a/1247828/180733
+timestamp=`sed -n                       's/^timestamp\s*=\s*\([0-9]*\)\s*$/\1/p'       $importMachineFile`
+importEdition=`sed -n               's/^importEdition\s*=\s*\([0-9a-zA-Z]*\)\s*$/\1/p' $importMachineFile`
+md5Tsv=`sed -n                             's/^md5Tsv\s*=\s*\([0-9a-f]*\)\s*$/\1/p'    $importMachineFile`
+md5Tables=`sed -n                       's/^md5Tables\s*=\s*\([0-9a-f]*\)\s*$/\1/p'    $importMachineFile`
+importStartHourFirst=`sed -n 's/^importStartHourFirst\s*=\s*\([0-9]*\)\s*$/\1/p'       $importMachineFile`
+importStartHourLast=`sed -n   's/^importStartHourLast\s*=\s*\([0-9]*\)\s*$/\1/p'       $importMachineFile`
+
+# Ensure the key variables are specified
+if [ -z "$timestamp" -o -z "$importEdition" -o -z "$md5Tsv" -o -z "$md5Tables" ]; then
+	echo "# The routing definition file does not contain all of timestamp,importEdition,md5Tsv,md5Tables"
+	exit 1
+fi
+
+# If specified, only allow this script to run between the specified times as the download can be large and disrupt main site performance
+if [ -n "$importStartHourLast" -a -n "$importStartHourFirst" ]; then
+	hour=$(date +%H)
+	if [ $hour -gt $importStartHourLast -o $hour -lt $importStartHourFirst ]; then
+		echo "# The specified import machine only permits downloads between ${importStartHourFirst}:00 and ${importStartHourLast}:59, to avoid disrupting main site performance"
+		exit
+	fi
+fi
+
+
+
+
 
 # This file identifies the import transfer script
 filename=xfer.sh
@@ -84,7 +119,7 @@ test="test -e ${filepath} -a \$(stat -c%s ${filepath}) -gt 0 -a \$(date -r ${fil
 set +e
 
 # Run the test - which will set $? to zero if it succeeds. Other values indicate failure or error.
-result=$(ssh ${importMachine} ${test})
+result=$(ssh ${importMachineAddress} ${test})
 
 # If the test succeeds, then check the second part
 if [ ! $? = 0 ]
@@ -98,7 +133,7 @@ set -e
 
 # Download
 echo "#	Downloading new version"
-scp ${importMachine}:${filepath} ${websitesBackupsFolder}/
+scp ${importMachineAddress}:${filepath} ${websitesBackupsFolder}/
 
 # Make sure it is executable
 chmod a+x ${filepath}
@@ -117,31 +152,31 @@ md5Tables=623950cc0a7e1a47c543d138e60be4bd
 
 
 # Start the transfer
-echo "#	Transferring the routing files from the import machine ${importMachine}:"
+echo "#	Transferring the routing files from the import machine ${importMachineAddress}:"
 
 # TSV file
 echo "#	Transfer the TSV file"
-scp ${importMachine}:${websitesBackupsFolder}/${importEdition}tsv.tar.gz ${websitesBackupsFolder}/
+scp ${importMachineAddress}:${websitesBackupsFolder}/${importEdition}tsv.tar.gz ${websitesBackupsFolder}/
 date
 
 # Hot-copied tables file
 echo "#	Transfer the hot copied tables file"
-scp ${importMachine}:${websitesBackupsFolder}/${importEdition}tables.tar.gz ${websitesBackupsFolder}/
+scp ${importMachineAddress}:${websitesBackupsFolder}/${importEdition}tables.tar.gz ${websitesBackupsFolder}/
 date
 
 # Sieve file
 #!# This is in a different place and could presumably be out-of-sync
 echo "#	Transfer the sieve"
-scp ${importMachine}:${websitesContentFolder}/import/sieve.sql ${websitesBackupsFolder}/
+scp ${importMachineAddress}:${websitesContentFolder}/import/sieve.sql ${websitesBackupsFolder}/
 
 # Installation script
 echo "#	Installation script"
-scp -p ${importMachine}:${websitesBackupsFolder}/irdb.sh ${websitesBackupsFolder}/
+scp -p ${importMachineAddress}:${websitesBackupsFolder}/irdb.sh ${websitesBackupsFolder}/
 date
 
 # Photos index and installer file
-scp ${importMachine}:${websitesBackupsFolder}/photoIndex.gz ${websitesBackupsFolder}/
-scp -p ${importMachine}:${websitesBackupsFolder}/installPhotoIndex.sh ${websitesBackupsFolder}/
+scp ${importMachineAddress}:${websitesBackupsFolder}/photoIndex.gz ${websitesBackupsFolder}/
+scp -p ${importMachineAddress}:${websitesBackupsFolder}/installPhotoIndex.sh ${websitesBackupsFolder}/
 echo "#	File transfer stage complete"
 
 
@@ -175,12 +210,12 @@ mysql ${importEdition} -hlocalhost -uroot -p${mysqlRootPassword} < /websites/www
 date
 
 echo "#	Unpack and install the tsv files."
-sudo -u cyclestreets tar xf ${websitesBackupsFolder}/${importEdition}tsv.tar.gz -C /websites/www/content/
+sudo -u $username tar xf ${websitesBackupsFolder}/${importEdition}tsv.tar.gz -C /websites/www/content/
 
 echo "#	Point current at new data:"
 #!# Replace/add the new daemon config file mechanism
 rm /websites/www/content/data/routing/current
-sudo -u cyclestreets ln -s ${importEdition}/ /websites/www/content/data/routing/current
+sudo -u $username ln -s ${importEdition}/ /websites/www/content/data/routing/current
 
 echo "#	Clean up the compressed tsv data."
 rm ${websitesBackupsFolder}/${importEdition}tsv.tar.gz
@@ -215,7 +250,7 @@ echo "#	Install the optimized nearestPoint table"
 mysql ${importEdition} -hlocalhost -uroot -p${mysqlRootPassword} -e "call createPathForNearestPoint();"
 
 echo "#	Install the sieve"
-sudo -u cyclestreets mv ${websitesBackupsFolder}/sieve.sql /websites/www/content/import/
+sudo -u $username mv ${websitesBackupsFolder}/sieve.sql /websites/www/content/import/
 
 echo "#	Building the photosEnRoute tables - but skipping the actual indexing"
 mysql ${importEdition} -hlocalhost -uroot -p${mysqlRootPassword} -e "call indexPhotos(true,0);"
@@ -237,9 +272,9 @@ date
 
 # Install photo index
 #!# Some of these "sudo -u cyclestreets" are not required
-sudo -u cyclestreets gunzip ${websitesBackupsFolder}/photoIndex.gz
+sudo -u $username gunzip ${websitesBackupsFolder}/photoIndex.gz
 #!# Fix this rename upstream
-sudo -u cyclestreets mv ${websitesBackupsFolder}/photoIndex ${websitesBackupsFolder}/photoIndex.sql
+sudo -u $username mv ${websitesBackupsFolder}/photoIndex ${websitesBackupsFolder}/photoIndex.sql
 mysql $importEdition -hlocalhost -uroot -p${mysqlRootPassword} < ${websitesBackupsFolder}/photoIndex.sql
 
 # Clean up

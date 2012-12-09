c171e05e7e5573cc3f4f868fcb40e37cb4c55d4f
mvl22 <martin@lucas-smith.co.uk>
2012-12-09 18:40:40 +0000

    Initial switchover script

---
+++ switch-routing-edition/run.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+# Script to install CycleStreets routing data on Ubuntu
+# Tested on 12.10 (View Ubuntu version using 'lsb_release -a')
+# This script is idempotent - it can be safely re-run without destroying existing data
+
+
+
+### Stage 1 - general setup
+
+echo "#	CycleStreets routing data installation switching"
+
+# Ensure this script is run as root
+if [ "$(id -u)" != "0" ]; then
+    echo "#	This script must be run as root." 1>&2
+    exit 1
+fi
+
+# Bomb out if something goes wrong
+set -e
+
+# Set a lock file; see: http://stackoverflow.com/questions/7057234/bash-flock-exit-if-cant-acquire-lock/7057385
+(
+	flock -n 9 || { echo 'An installation is already running' ; exit 1; }
+
+
+### CREDENTIALS ###
+
+# Define the location of the credentials file; see: http://stackoverflow.com/a/246128/180733
+configFile=../.config.sh
+SCRIPTDIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Generate your own credentials file by copying from .config.sh.template
+if [ ! -e $SCRIPTDIRECTORY/${configFile} ]; then
+    echo "# The config file, ${configFile}, does not exist - copy your own based on the ${configFile}.template file." 1>&2
+    exit 1
+fi
+
+# Load the credentials
+. $SCRIPTDIRECTORY/${configFile}
+
+# Logging
+# Use an absolute path for the log file to be tolerant of the changing working directory in this script
+setupLogFile=$(readlink -e $(dirname $0))/log.txt
+touch ${setupLogFile}
+echo "#	CycleStreets routing data switchover in progress, follow log file with: tail -f ${setupLogFile}"
+echo "#	CycleStreets routing data switchover $(date)" >> ${setupLogFile}
+
+# Ensure there is a cyclestreets user account
+if [ ! id -u ${username} >/dev/null 2>&1 ]; then
+	echo "# User ${username} must exist: please run the main website install script"
+	exit 1
+fi
+
+# Ensure the main website installation is present
+if [ ! -d ${websitesContentFolder}/data/routing -o ! -d $websitesBackupsFolder ]; then
+	echo "# The main website installation must exist: please run the main website install script"
+	exit 1
+fi
+
+# Ensure the routing daemon (service) is installed
+if [ ! -f /etc/init.d/cycleroutingd ]; then
+	echo "The routing daemon (service) is not installed"
+	exit 1
+fi
+
+
+### Stage 2 - ensure required parameters are present
+
+# Ensure there is a single argument, defining the routing edition, or end
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` importedition"
+  exit 1
+fi
+
+# Allocate that argument
+importEdition=$1
+
+
+### Stage 3 - confirm existence of the routing import database and files
+
+# Check to see that this routing database exists
+if ! mysql -hlocalhost -uroot -p${mysqlRootPassword} -e "use ${importEdition}"; then
+	echo "The routing database ${importEdition} is not present"
+	exit 1
+fi
+
+# Check to see that the routing data file for this routing edition exists
+if [ ! -d "${websitesContentFolder}/data/routing/${importEdition}" ]; then
+	echo "The routing data file ${importEdition} is not present"
+	exit 1
+fi
+
+
+### Stage 4 - do switch-over
+
+# Put the site into maintenance mode
+sudo -u $username touch ${websitesContentFolder}/maintenance
+
+# Stop the service if running
+ps cax | grep routing_server.py > /dev/null
+if [ $? -eq 0 ]; then
+	echo "Stopping current routing service"
+	service cycleroutingd stop
+fi
+
+#!# Needs to wait for it to start somehow
+
+
+# Start the routing daemon (service)
+service cycleroutingd start
+
+# Switch the website to the new routing database
+mysql cyclestreets -hlocalhost -uroot -p${mysqlWebsiteUsername} -e "UPDATE map_config SET routingDb = '${importEdition}' WHERE id = 1;";
+
+# Restore the site by switching off maintenance mode
+rm ${websitesContentFolder}/maintenance
+
+
+### Stage 5 - end
+
+# Finish
+date
+echo "All done"
+
+# Remove the lock file
+) 9>/var/lock/cyclestreetsimport
+
+

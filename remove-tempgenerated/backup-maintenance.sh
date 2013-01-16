63a4b2506a688d823cb1bfdd875eb840f0c0f14e
si-the-pie <simon.nuttall@gmail.com>
2013-01-16 14:54:12 +0000

    Remove sudo from svn update as the script is not run as root anyway

--- remove-tempgenerated/backup-maintenance.sh
+++ remove-tempgenerated/backup-maintenance.sh
@@ -2,11 +2,15 @@
 #
 # This is a daily maintenance script for backup deployments of CycleStreets.
 
-#	User cyclestreets (rather than root) for safety
-asCS="sudo -u cyclestreets"
+# Ensure this script is NOT run as root (it should be run as cyclestreets)
+if [ "$(id -u)" = "0" ]; then
+    echo "#	This script must NOT be run as root." 1>&2
+    exit 1
+fi
 
 #	Make sure the latest code is present
-${asCS} svn update /websites/www/content/
+svn update /websites/www/content/
+
 
 #	Delete previously generated routing database, tables and tsv files to conserve space.
 # The following find command selects top-level files in the given path that are older than 24 hours and deletes them.
@@ -18,7 +22,7 @@ ${asCS} svn update /websites/www/content/
 # -type f finds only files
 
 # !! Needs checking whether this is still a useful/relevant cleanup
-# ${asCS} find /websites/www/backups -maxdepth 1 -type f -name "routing*\.gz" \! -mtime 0 -type f -delete
+# find /websites/www/backups -maxdepth 1 -type f -name "routing*\.gz" \! -mtime 0 -type f -delete
 
 
 # End of file

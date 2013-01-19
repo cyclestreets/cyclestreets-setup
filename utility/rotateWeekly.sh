a72cbb433607d4dc08aa02e9586b4cad24725a3d
si-the-pie <simon.nuttall@gmail.com>
2013-01-19 02:58:07 +0000

    Rotate weekly utility

---
+++ utility/rotateWeekly.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#	This script should run on the backup machine, every week.
+#	It rotates the backups relative to the folder
+#	The arguments are:
+#	1. folder relative to root (not slash terminated) e.g. /websites/cyclescape/backup
+#	2. name of the archive file e.g. toolkitShared.tar.bz2
+
+folder=$1
+archive=$2
+
+#	Folder locations
+weeklyFolder=${folder}/rotation/weekly/$(date +"%V")
+
+#	Recreate the new backup folder - if necessary
+mkdir -p ${weeklyFolder}
+
+#	Update the backup folder modified time - so its easy to see which is the newest
+touch ${weeklyFolder}
+
+#	Copy the backup to that folder
+cp -p ${folder}/${archive} ${weeklyFolder}
+
+# End of file

9ef55c42f49882b78b038b333fba19ad9d6a2d76
si-the-pie <simon.nuttall@gmail.com>
2013-01-19 02:48:33 +0000

    Configuration: cyclestreets weekly backup rotation

---
+++ daily-backup/cyclestreetsRotateWeekly.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#	Rotates the CycleStreets backups weekly.
+
+### Stage 1 - general setup
+
+# Ensure this script is NOT run as root (it should be run as cyclestreets)
+if [ "$(id -u)" = "0" ]; then
+    echo "#	This script must NOT be run as root." 1>&2
+    exit 1
+fi
+
+# Bomb out if something goes wrong
+set -e
+
+# Lock directory
+lockdir=/var/lock/cyclestreets
+mkdir -p $lockdir
+
+# Set a lock file; see: http://stackoverflow.com/questions/7057234/bash-flock-exit-if-cant-acquire-lock/7057385
+(
+	flock -n 9 || { echo 'CycleStreets weekly rotate is already in progress' ; exit 1; }
+
+### CREDENTIALS ###
+
+# Get the script directory see: http://stackoverflow.com/a/246128/180733
+# The multi-line method of geting the script directory is needed because this script is likely symlinked from cron
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SCRIPTDIRECTORY=$DIR
+
+# Define the location of the credentials file relative to script directory
+configFile=../.config.sh
+
+# Generate your own credentials file by copying from .config.sh.template
+if [ ! -e $SCRIPTDIRECTORY/${configFile} ]; then
+    echo "# The config file, ${configFile}, does not exist - copy your own based on the ${configFile}.template file." 1>&2
+    exit 1
+fi
+
+# Load the credentials
+. $SCRIPTDIRECTORY/${configFile}
+
+
+# Main body
+
+#	Folder locations
+folder=${websitesBackupsFolder}
+rotateWeekly=${SCRIPTDIRECTORY}/../utility/rotateWeekly.sh
+
+#	Rotate
+$rotateWeekly $folder www_cyclestreets.sql.gz
+$rotateWeekly $folder www_schema_blog_database.sql.gz
+$rotateWeekly $folder www_schema_blogcyclescape_database.sql.gz
+$rotateWeekly $folder csTracBackup.tar.bz2
+$rotateWeekly $folder cyclestreetsRepo.dump.bz2
+
+# Remove the lock file
+) 9>$lockdir/cyclestreetsRotateWeekly
+
+# End of file

19d7807ea5b7aabbda2f1e2a484428b25798d2ca
si-the-pie <simon.nuttall@gmail.com>
2013-01-31 09:58:39 +0000

    InstallCronJobs function first argument identifies user's crontab

--- utility/helper.sh
+++ utility/helper.sh
@@ -3,12 +3,16 @@
 ## Helper functions
 
 #	Install cron jobs
-#	The first argument should be an array of cron jobs
+#	The first argument identifies which user's crontab
+#	The second argument names an array of cron jobs, ie. use: jobs[@] rather than $jobs
 installCronJobs ()
 {
+    # Shortcut for running commands as the suggested user
+    asUser="sudo -u $1"
+
     # http://stackoverflow.com/questions/1063347/passing-arrays-as-parameters-in-bash
-    # Uses the ${!...} notation to indirectly refer to the value of $1, the first argument.
-    declare -a jobs=("${!1}")
+    # Uses the ${!...} notation to indirectly refer to the value of $2, the second argument.
+    declare -a jobs=("${!2}")
 
     # Indicate number of jobs
     echo "#	Installing ${#jobs[@]} cron jobs"
@@ -25,7 +29,7 @@ installCronJobs ()
 	# Install/update the job
 	# frgrep -v .. <(${} crontab -l) filters out any previous occurrences from the user's crontab listing
 	# The echo adds the new job and the cat | pipes it to set the user's updated crontab
-	cat <(fgrep -i -v "$command" <(${asCS} crontab -l)) <(echo "$job") | ${asCS} crontab -
+	cat <(fgrep -i -v "$command" <(${asUser} crontab -l)) <(echo "$job") | ${asUser} crontab -
 
 	# Report installed job
 	echo "#	Installed ${job}"

5f6bb40e39a28a39be249d003d14ab6ec7eaa5d4
si-the-pie <simon.nuttall@gmail.com>
2013-02-08 12:56:28 +0000

    Inserted missing websites content folder ref.

--- install-website/run.sh
+++ install-website/run.sh
@@ -168,7 +168,7 @@ fi
 # Allow the Apache webserver process to write / add to the data/ folder
 chown -R www-data ${websitesContentFolder}/data
 # Fix ownership of the svn folder and its contents back to username, to allow svn updating
-find data -name .svn -a -type d -print0 | xargs -0 chown -R ${username}
+find ${websitesContentFolder}/data -name .svn -a -type d -print0 | xargs -0 chown -R ${username}
 
 # For gpsPhoto.pl (for geolocation by synchronization), add dependencies, and Ensure the webserver (and group, but not others ideally) have executability on gpsPhoto.pl
 apt-get -y install libimage-exiftool-perl

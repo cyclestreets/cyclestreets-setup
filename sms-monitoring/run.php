41b9bb70789175c58fb7957ec98138b186bb15a3
root <root@cyclestreets.net>
2013-03-30 14:21:21 +0000

    Add ignore times to avoid SMS at 4.15am

--- sms-monitoring/run.php
+++ sms-monitoring/run.php
@@ -42,7 +42,15 @@ class doCheck
 		# Set the timeout for URL requests
 		ini_set ('default_socket_timeout', $this->timeoutSeconds);
 		
-		# Get the registered checks in this classa
+		# Ignore times if required
+		if ($ignoreTimes) {
+			$currentTime = date ('Hi');
+			if (in_array ($currentTime, $ignoreTimes)) {
+				return;
+			}
+		}
+		
+		# Get the registered checks in this class
 		$methods = get_class_methods ($this);
 		$tests = array ();
 		foreach ($methods as $index => $method) {
@@ -142,7 +150,7 @@ class doCheck
 			|| !isSet ($result['marker'][0])
 			|| !isSet ($result['marker'][0]['@attributes'])
 			|| !isSet ($result['marker'][0]['@attributes']['start'])
-			|| ($result['marker'][0]['@attributes']['start'] != 'Buckingham Palace')
+			|| ($result['marker'][0]['@attributes']['start'] != '_Buckingham Palace')
 			
 			# Check for a co-ordinate in the right area of the country
 			|| !isSet ($result['marker'][0]['@attributes']['coordinates'])

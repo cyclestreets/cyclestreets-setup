d2dcda8818b4eae6aef99ca50072831bfe9af9c8
si-the-pie <simon.nuttall@gmail.com>
2013-04-16 20:19:56 +0100

    Added failover reversal files

---
+++ failover-deployment/toViola.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+#	When olivia is running as the live server, this script can be used to generate files to keep viola in sync.
+#	And should be run manually on olivia, as cyclestreets
+
+### Stage 1 - general setup
+
+# Ensure this script is NOT run as root (it should be run as cyclestreets)
+if [ "$(id -u)" = "0" ]; then
+    echo "#	This script must NOT be run as root." 1>&2
+    exit 1
+fi
+
+# Bomb out if something goes wrong
+set -e
+
+### CREDENTIALS ###
+
+# Get the script directory see: http://stackoverflow.com/a/246128/180733
+# The multi-line method of geting the script directory is needed because this script is likely symlinked from cron
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SCRIPTDIRECTORY=$DIR
+
+# Define the location of the credentials file relative to script directory
+configFile=../.config.sh
+
+# Generate your own credentials file by copying from .config.sh.template
+if [ ! -x $SCRIPTDIRECTORY/${configFile} ]; then
+    echo "# The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file." 1>&2
+    exit 1
+fi
+
+# Load the credentials
+. $SCRIPTDIRECTORY/${configFile}
+
+# Logging
+setupLogFile=$SCRIPTDIRECTORY/log.txt
+touch ${setupLogFile}
+echo "#	CycleStreets toViola in progress, follow log file with: tail -f ${setupLogFile}"
+echo "$(date)	CycleStreets toViola $(id)" >> ${setupLogFile}
+
+# Ensure live machine has been defined
+if [ -z "${liveMachineAddress}" ]; then
+    echo "# A live machine must be defined in order to run updates" >> ${setupLogFile}
+    exit 1
+fi
+
+# Ensure there is a cyclestreets user account
+if [ ! id -u ${username} >/dev/null 2>&1 ]; then
+	echo "$(date) User ${username} must exist: please run the main website install script" >> ${setupLogFile}
+	exit 1
+fi
+
+# Ensure this script is run as cyclestreets user
+if [ ! "$(id -nu)" = "${username}" ]; then
+    echo "#	This script must be run as user ${username}, rather than as $(id -nu)." 1>&2
+    exit 1
+fi
+
+# Ensure the main website installation is present
+if [ ! -d ${websitesContentFolder}/data/routing -o ! -d $websitesBackupsFolder ]; then
+	echo "$(date) The main website installation must exist: please run the main website install script" >> ${setupLogFile}
+	exit 1
+fi
+
+### Stage 2
+
+#	IJS tables
+#	Close the journey planner to stop new itineraries being made while we archive the current IJS tables
+mysql cyclestreets -hlocalhost -uroot -p${mysqlRootPassword} -e "update map_config set journeyPlannerStatus='closed',whenStatusChanged=NOW(),notice='Brief closure to archive Journeys.'";
+#
+#	Archive the IJS tables
+mysqldump --no-create-db --no-create-info --insert-ignore --skip-triggers -hlocalhost -uroot -p${mysqlRootPassword} cyclestreets map_itinerary map_journey map_segment map_wpt map_jny_poi map_street_hurdle map_error | gzip > /websites/www/backups/www_schema_ijs_tables.sql.gz
+#
+#	Repartition, which moves the current to the archived tables. See: documentation/schema/repartition.sql
+mysql cyclestreets -hlocalhost -uroot -p${mysqlRootPassword} -e "call repartitionIJS()";
+#
+#	Re-open the journey planner.
+mysql cyclestreets -hlocalhost -uroot -p${mysqlRootPassword} -e "update map_config set journeyPlannerStatus='live',notice=''";
+
+#	Create md5 hash
+openssl dgst -md5 /websites/www/backups/www_schema_ijs_tables.sql.gz > /websites/www/backups/www_schema_ijs_tables.sql.gz.md5
+
+# Tested to here
+exit 1
+#	Backup the CycleStreets database
+#	Option -R dumps stored procedures & functions
+mysqldump -hlocalhost -uroot -p${mysqlRootPassword} -R cyclestreets | gzip > /websites/www/backups/www_cyclestreets.sql.gz
+#	Create md5 hash
+openssl dgst -md5 /websites/www/backups/www_cyclestreets.sql.gz > /websites/www/backups/www_cyclestreets.sql.gz.md5
+
+# 	Schema Structure (no data)
+#	This allows the schema to be viewed at the page: http://www.cyclestreets.net/schema/sql/
+#	Option -R dumps stored procedures & functions
+mysqldump -R --no-data -hlocalhost -uroot -p${mysqlRootPassword} cyclestreets | gzip > /websites/www/backups/www_schema_cyclestreets.sql.gz
+#	Create md5 hash
+openssl dgst -md5 /websites/www/backups/www_schema_cyclestreets.sql.gz > /websites/www/backups/www_schema_cyclestreets.sql.gz.md5
+
+
+#	Blogs
+#	These databases do not have any stored routines, so the -R option is not necessary
+
+#	Dump
+mysqldump -hlocalhost -uroot -p${mysqlRootPassword} blog | gzip > /websites/www/backups/www_schema_blog_database.sql.gz
+#	Hash
+openssl dgst -md5 /websites/www/backups/www_schema_blog_database.sql.gz > /websites/www/backups/www_schema_blog_database.sql.gz.md5
+
+#	Dump
+mysqldump -hlocalhost -uroot -p${mysqlRootPassword} blogcyclescape | gzip > /websites/www/backups/www_schema_blogcyclescape_database.sql.gz
+#	Hash
+openssl dgst -md5 /websites/www/backups/www_schema_blogcyclescape_database.sql.gz > /websites/www/backups/www_schema_blogcyclescape_database.sql.gz.md5
+
+#
+#	Clear out temp files which needs to be run as www-data for safety.
+sudo -u www-data ${websitesContentFolder}/data/tempgenerated/zap.sh
+
+### Final Stage
+
+# Finish
+echo "$(date)	All done" >> ${setupLogFile}
+
+# End of file

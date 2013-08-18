8c38aae6e0af0ae8312afbc494c83f35de9e7ca0
simon <simon.nuttall@gmail.com>
2013-08-18 10:28:49 +0100

    Scripts for managing schema

---
+++ schema-scripts/externalDb.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Script to manage CycleStreets schema
+#
+# Tested on 13.04 View Ubuntu version using: lsb_release -a
+
+echo "#	CycleStreets schema script $(date)"
+
+# Ensure this script is not run as root
+if [ "$(id -u)" == "0" ]; then
+    echo "#	This script must not be run as root." 1>&2
+    exit 1
+fi
+
+# Bomb out if something goes wrong
+set -e
+
+### CREDENTIALS ###
+
+# Get the script directory see: http://stackoverflow.com/a/246128/180733
+# The second single line solution from that page is probably good enough as it is unlikely that this script itself will be symlinked.
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTDIRECTORY=$DIR
+
+# Name of the credentials file
+configFile=../.config.sh
+
+# Generate your own credentials file by copying from .config.sh.template
+if [ ! -x ./${configFile} ]; then
+    echo "#	The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file." 1>&2
+    exit 1
+fi
+
+# Load the credentials
+. ./${configFile}
+
+# Shortcut for running commands as the cyclestreets user
+asCS="sudo -u ${username}"
+
+# Logging
+# Use an absolute path for the log file to be tolerant of the changing working directory in this script
+echo "#	CycleStreets schema script starting"
+
+# Main Body
+
+# Create the schema file
+# Uses some options and a trick with sed to make the schema file avoid overwriting any existing data.
+mysqldump -hlocalhost -uroot -p${mysqlRootPassword} csExternal --databases --no-data --skip-add-drop-table | sed 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g' > ${websitesContentFolder}/documentation/schema/csExternal.sql
+
+# Create the version with data
+if [ ! -z "${csExternalDataFile}" ]; then
+    mysqldump -hlocalhost -uroot -p${mysqlRootPassword} csExternal | gzip > ${websitesBackupsFolder}/${csExternalDataFile}
+fi
+
+# Confirm end of script
+echo "#	Script completed $(date)"
+
+# Return true to indicate success
+:
+
+# End of file

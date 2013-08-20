d9d1508b8f42f990994229aab053e965476901dc
simon <simon.nuttall@gmail.com>
2013-08-20 12:31:35 +0100

    MySQL import config moved from install to deploy

--- install-import/run.sh
+++ install-import/run.sh
@@ -170,66 +170,6 @@ if [ -n "${csExternalDataFile}" -a ! -r ${websitesBackupsFolder}/${csExternalDat
 	gunzip < ${websitesBackupsFolder}/${csExternalDataFile} | ${mysql} ${externalDb}
 fi
 
-
-# MySQL configuration
-mysqlConfFile=/etc/mysql/conf.d/cyclestreets.cnf
-if [ ! -r ${mysqlConfFile} ]; then
-
-    # Create the file (avoid any backquotes in the text as they'll spawn sub-processes)
-    cat > ${mysqlConfFile} <<EOF
-# MySQL Configuration for import server
-# This config should be loaded via a symlink from: /etc/mysql/conf.d/
-# On systems running apparmor the symlinks need to be enabled via /etc/apparmor.d/usr.sbin.mysqld
-
-# Main characteristics
-# * Handle very large tables
-# * Long group_concat
-
-[mysqld]
-
-# General options as recommended by
-# http://www.percona.com/pdf-canonical-header?path=files/presentations/percona-live/dc-2012/PLDC2012-optimizing-mysql-configuration.pdf
-# mysqltuner
-# select @@thread_cache_size, @@table_open_cache, @@open_files_limit;
-thread_cache_size = 100
-table_open_cache = 4096
-open_files_limit = 65535
-
-# This should be set to about 20 - 50% of available memory. On our 8GB www machine a good size is probably 1G. (The default is only 16M is a performance killer.)  
-key_buffer		= 4G
-
-max_allowed_packet	= 16M
-group_concat_max_len	= 50K
-
-# These are quite big
-query_cache_limit	= 1M
-query_cache_size        = 50M
-
-log_slow_queries	= /var/log/mysql/mysql-slow.log
-long_query_time = 3
-
-# The following setting was added following getting this error during an import run:
-#     ERROR 1206 (HY000): The total number of locks exceeds the lock table size
-# Even though the database did not contain any innodb tables, it did fix the problem with an update to text columns in a table with almost 9 million rows.
-innodb_buffer_pool_size=64MB
-
-# CHARACTER SET
-# It is simplest (and quickest, due to no translation overhead) if all text uses the utf8 character set and collation utf8_unicode_ci (case-insensitive).
-# Set these in the mysql server configuration so that the osmosis program which reads the OpenStreetMap planet extracts also uses this character set.
-
-# Set default character set and collation
-character_set_server=utf8
-collation_server=utf8_unicode_ci
-EOF
-
-    # Allow the user to edit this file
-    chown ${username}:rollout ${mysqlConfFile}
-fi
-
-
-# Advise
-echo "#	MySQL configured, but consider running the following security step from the command line: mysql_secure_installation"
-
 # Confirm end of script
 echo "#	All now installed $(date)"
 

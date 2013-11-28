7414527d2f741cc8d29b9a7c017a754d2c360f5e
simon <simon.nuttall@gmail.com>
2013-11-28 11:17:49 +0000

    Looser geocoding tolerance fix 2

--- sms-monitoring/run.php
+++ sms-monitoring/run.php
@@ -324,8 +324,8 @@ class doCheck
 			
 			# Check for a co-ordinate in the right area of the country
 			|| (!substr_count ($thodayResult['name'], 'Thoday'))
-			|| abs ($thodayResult['longitude'] - 0.14) < 0.1
-			|| abs ($thodayResult['latitude'] - 52.20) < 0.1
+			|| abs ($thodayResult['longitude'] - 0.14) > 0.1
+			|| abs ($thodayResult['latitude'] - 52.20) > 0.1
 			
 			# Testing...
 		    #|| true

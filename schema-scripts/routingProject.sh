633245fce5bd9201a21900795a293d3c46c780d0
simon <simon.nuttall@gmail.com>
2014-05-13 20:56:41 +0100

    Uncomment out dumpline

---
+++ schema-scripts/routingProject.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Script to produce a sample routing database dump for an academic project
+
+echo "#	CycleStreets schema script $(date)"
+
+# Ensure this script is not run as root
+if [ "$(id -u)" == "0" ]; then
+    echo "#	This script must not be run as root." 1>&2
+    exit 1
+fi
+
+# Bomb out if something goes wrong
+set -e
+
+### CREDENTIALS ###
+
+# Get the script directory see: http://stackoverflow.com/a/246128/180733
+# The second single line solution from that page is probably good enough as it is unlikely that this script itself will be symlinked.
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTDIRECTORY=$DIR
+
+# Name of the credentials file
+configFile=../.config.sh
+
+# Generate your own credentials file by copying from .config.sh.template
+if [ ! -x ./${configFile} ]; then
+    echo "#	The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file." 1>&2
+    exit 1
+fi
+
+# Load the credentials
+. ./${configFile}
+
+# Shortcut for running commands as the cyclestreets user
+asCS="sudo -u ${username}"
+
+# Report
+echo "#	CycleStreets schema script starting"
+
+# Main Body
+credentials="-hlocalhost -uroot -p${mysqlRootPassword}"
+
+# The current database name will be the sample database
+sampleRoutingDb=$(mysql -s ${credentials} cyclestreets<<<"select routingDb from map_config limit 1")
+echo "# Using routing data from the db named: ${sampleRoutingDb}"
+
+#	Write
+mysqldump ${sampleRoutingDb} ${credentials} map_way map_routingFactor map_wayName | gzip > ${websitesContentFolder}/documentation/schema/routingProject.sql.gz
+
+#	Advise
+echo "#	Actions required next:"
+echo "#	End user will need to create a database and load the data into it using:"
+echo "mysql -e \"create database ${sampleRoutingDb} default character set utf8 default collate utf8_unicode_ci;\""
+echo "gunzip < routingProject.sql.gz | mysql ${sampleRoutingDb}"
+
+# Confirm end of script
+echo "#	Script completed $(date)"
+
+# Return true to indicate success
+:
+
+# End of file

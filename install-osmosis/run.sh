641762f8573b46008b6de61042116f78c9a6313c
si-the-pie <simon.nuttall@gmail.com>
2015-03-10 20:04:52 +0000

    Removed unnecessary credentials loading

--- install-osmosis/run.sh
+++ install-osmosis/run.sh
@@ -18,25 +18,6 @@ fi
 # Bomb out if something goes wrong
 set -e
 
-### CREDENTIALS ###
-
-# Get the script directory see: http://stackoverflow.com/a/246128/180733
-# The second single line solution from that page is probably good enough as it is unlikely that this script itself will be symlinked.
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCRIPTDIRECTORY=$DIR
-
-# Name of the credentials file
-configFile=../.config.sh
-
-# Generate your own credentials file by copying from .config.sh.template
-if [ ! -x ./${configFile} ]; then
-    echo "#	The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file." 1>&2
-    exit 1
-fi
-
-# Load the credentials
-. ./${configFile}
-
 # Osmosis requires java
 apt-get -y install openjdk-7-jre
 

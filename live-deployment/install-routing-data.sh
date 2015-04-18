c08d802962acac9110f738e408c397689683dbd1
si-the-pie <simon.nuttall@gmail.com>
2015-04-18 10:06:06 +0100

    Install routing data - updated test version comment

--- live-deployment/install-routing-data.sh
+++ live-deployment/install-routing-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to install CycleStreets routing data
-# Tested on Ubuntu 12.10 (View Ubuntu version using 'lsb_release -a')
+# Tested on Ubuntu 14.04 (View Ubuntu version using 'lsb_release -a')
 # This script is idempotent - it can be safely re-run without destroying existing data
 
 # Requires password-less access to the import machine.

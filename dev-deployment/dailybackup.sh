063ff43d227feed965efc59394a9322f63b2882c
si-the-pie <simon.nuttall@gmail.com>
2015-04-19 19:50:27 +0100

    daily-backup: avoid excessive use of sudo

--- dev-deployment/dailybackup.sh
+++ dev-deployment/dailybackup.sh
@@ -4,6 +4,7 @@
 #
 #       http://dev.cyclestreets.net/wiki/BackupStrategy
 #
+# Run as the cyclestreets user (a check is peformed after the config file is loaded).
 
 # Avoid echoing if possible as this generates cron messages
 # echo "#	CycleStreets Dev machine backup $(date)"
@@ -28,12 +29,27 @@ configFile=${ScriptHome}/.config.sh
 
 # Generate your own credentials file by copying from .config.sh.template
 if [ ! -x ${configFile} ]; then
-    echo "#	The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file." 1>&2
+    echo "#	The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file."
     exit 1
 fi
 
 # Load the credentials
 . ${configFile}
+
+# Main body of script
+
+# Ensure this script is run as cyclestreets user
+if [ ! "$(id -nu)" = "${username}" ]; then
+    echo "#	This script must be run as user ${username}, rather than as $(id -nu)."
+    exit 1
+fi
+
+#	Check the backups folder is not empty
+if [ -z "${websitesBackupsFolder}" ]; then
+    echo "#	The backups folder is empty."
+    exit 1
+fi
+
 
 # Logging
 # Use an absolute path for the log file to be tolerant of the changing working directory in this script
@@ -44,41 +60,40 @@ echo "#	CycleStreets Dev backup $(date)" >> ${setupLogFile}
 # Bomb out if something goes wrong
 set -e
 
-#	Location of backup folder
-backups=/websites/www/backups
-
-
 ##       Trac
 dump=csTracBackup.tar.bz2
 
 #	Park the old dump
-if [ -d ${backups}/trac ]; then
-    mv ${backups}/trac ${backups}/tracOld
+if [ -d ${websitesBackupsFolder}/trac ]; then
+    mv ${websitesBackupsFolder}/trac ${websitesBackupsFolder}/tracOld
 fi
 
 #	Dump
-echo $password | sudo -S trac-admin /websites/dev/trac/cyclestreets hotcopy ${backups}/trac >> ${setupLogFile}
-echo $password | sudo -S chown -R cyclestreets ${backups}/trac
+trac-admin /websites/dev/trac/cyclestreets hotcopy ${websitesBackupsFolder}/trac >> ${setupLogFile}
 
 #	Archive
-tar cjf ${backups}/${dump} -C ${backups} trac
+tar cjf ${websitesBackupsFolder}/${dump} -C ${websitesBackupsFolder} trac
 
 #     Hash
-openssl dgst -md5 ${backups}/${dump} > ${backups}/${dump}.md5
+openssl dgst -md5 ${websitesBackupsFolder}/${dump} > ${websitesBackupsFolder}/${dump}.md5
 
 #	Tidyup
-if [ -d ${backups}/tracOld ]; then
-    echo $password | sudo -S rm -rf ${backups}/tracOld
+if [ -d ${websitesBackupsFolder}/tracOld ]; then
+    # !! Very dodgy command
+    rm -rf ${websitesBackupsFolder}/tracOld
 fi
+
+## !! WIP 19 Apr 2015 exit cleanly
+exit 0
 
 ##	Subversion
 dump=cyclestreetsRepo.dump.bz2
 
 #      Dump
 #	This takes almost half an hour in mid Feb 2012, and an hour in Jan 2013, with size 900M.
-echo $password | sudo -S svnadmin dump /websites/svn/svn/cyclestreets -q | bzip2 > ${backups}/${dump}
+echo $password | sudo -S svnadmin dump /websites/svn/svn/cyclestreets -q | bzip2 > ${websitesBackupsFolder}/${dump}
 #	Hash
-openssl dgst -md5 ${backups}/${dump} > ${backups}/${dump}.md5
+openssl dgst -md5 ${websitesBackupsFolder}/${dump} > ${websitesBackupsFolder}/${dump}.md5
 
 
 #	Finally

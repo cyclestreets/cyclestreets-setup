a72688f0eedbba90326b7e98db38f76d304240c8
si-the-pie <simon.nuttall@gmail.com>
2015-05-04 11:35:31 +0100

    Dev: passwordless svnadmin command revert previous 'fix' which in fact broke

--- dev-deployment/deploy.sh
+++ dev-deployment/deploy.sh
@@ -52,7 +52,7 @@ if [ -n "${csSudoers}" -a ! -e "${csSudoers}" ]; then
     cat > ${csSudoers} << EOF
 # Dev deployment
 # Permit cyclestreets user to dump svn without a password
-cyclestreets ALL = (root) NOPASSWD: svnadmin dump
+cyclestreets ALL = (root) NOPASSWD: /usr/bin/svnadmin dump
 EOF
 
     # Make it read only

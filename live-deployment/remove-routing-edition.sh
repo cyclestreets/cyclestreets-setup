15c9564da31152e7c759f183697e94578886a1b5
si-the-pie <simon.nuttall@gmail.com>
2015-12-19 19:43:57 +0000

    removal script requires an argument

--- live-deployment/remove-routing-edition.sh
+++ live-deployment/remove-routing-edition.sh
@@ -11,16 +11,16 @@ usage()
 {
     cat << EOF
 SYNOPSIS
-	$0 -h -q [routingEdition]
+	$0 -h -q routingEdition
 
 OPTIONS
 	-h Show this message
-	-q Suppress helpful messages, error messages are still produced
+	-q Suppress narrative messages, error messages are still produced
 
 DESCRIPTION
 	routingEdition
 		Names a routing database of the form routingYYMMDD, eg. routing151205
-		Defaults to the oldest version avaialble.
+		Alternatively the words newest or oldest can be used.
 EOF
 }
 
@@ -99,27 +99,47 @@ fi
 
 ## Main body from here
 
-# Check the supplied argument - if exactly one use it, else default to latest routing db
-if [ $# -eq 1 ]
+# Check there is exactly one arugment - the routing edition
+if [ $# -ne 1 ]
 then
-
-    # Allocate that argument
-    oldEdition=$1
-else
-
+    echo "#	The only permitted argument is the edition to remove."
+    exit 1
+fi
+    
+# Check the supplied argument
+if [ "$1" = 'oldest' -o "$1" = 'newest' ]
+then
+    # Find the oldest edition
     # Count the number of routing editions not including the null edition
     numEditions=$(${superMysql} -s cyclestreets<<<"SELECT count(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME != 'routing000000' and SCHEMA_NAME LIKE 'routing%';")
 
-    # Check that there are at least three routing editions - to avoid removing the latest ones.
+    # Check that there are at least three routing editions - to avoid removing the most recent ones
     if [ -z "${numEditions}" -o "${numEditions}" -lt 3 ]
     then
-	vecho "# There are ${numEditions} editions which is too few to use the oldest as a default value."
+	vecho "# There are ${numEditions} editions which is too few to use the oldest/newest argument."
 	exit 1
     fi
 
+    # Select sort order
+    [[ "$1" = 'oldest' ]] && sort='asc' || sort='desc'
+
     # Determine oldest edition not including the null edition (the -s suppresses the tabular output)
-    oldEdition=$(${superMysql} -s cyclestreets<<<"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME != 'routing000000' and SCHEMA_NAME LIKE 'routing%' order by SCHEMA_NAME asc limit 1;")
+    removeEdition=$(${superMysql} -s cyclestreets<<<"SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME != 'routing000000' and SCHEMA_NAME LIKE 'routing%' order by SCHEMA_NAME ${sort} limit 1;")
+
+else
+
+    # Use the provided edition
+    removeEdition=$1
 fi
+
+# Check the format is routingYYMMDD
+if [[ ! "$removeEdition" =~ routing([0-9]{6}) ]]; then
+  echo "#	The supplied argument must specify a routing edition of the form routingYYMMDD, but this was received: ${removeEdition}."
+  exit 1
+fi
+
+# Extract the date part of the routing database
+editionDate=${BASH_REMATCH[1]}
 
 # Ensure there is a cyclestreets user account
 if [ ! id -u ${username} >/dev/null 2>&1 ]; then
@@ -154,7 +174,7 @@ fi
 # XML for the calls to get the routing edition
 xmlrpccall="<?xml version=\"1.0\" encoding=\"utf-8\"?><methodCall><methodName>get_routing_edition</methodName></methodCall>"
 
-# Check the local routing service to make sure that it won't be deletes - but it is not a requirement that it is currently serving routes.
+# Check the local routing service to make sure that it won't be deleted - but it is not a requirement that it is currently serving routes.
 # The status check produces an error if it is not running, so briefly turn off abandon-on-error to catch and report the problem.
 set +e
 
@@ -162,7 +182,7 @@ set +e
 localRoutingStatus=$(${routingDaemonLocation} status)
 if [ $? -ne 0 ]
 then
-  vecho "#	Note: there is no current routing service. Routing edition ${oldEdition} removal will proceed."
+  vecho "#	Note: there is no current routing service. Routing edition ${removeEdition} removal will proceed."
 else
 
     # Check not already serving this edition
@@ -176,8 +196,8 @@ else
     fi
 
     # Check the fallback routing edition is the same as the proposed edition
-    if [ "${oldEdition}" == "${currentRoutingEdition}" ]; then
-	vecho "#	The proposed edition to remove: ${oldEdition} is currently being served from ${localRoutingServer}"
+    if [ "${removeEdition}" == "${currentRoutingEdition}" ]; then
+	vecho "#	The proposed edition to remove: ${removeEdition} is currently being served from ${localRoutingServer}"
 	vecho "#	Stop it using: sudo service cycleroutingd stop"
 	exit 1
     fi
@@ -186,31 +206,22 @@ fi
 # Restore abandon-on-error
 set -e
 
-# Check the format is routingYYMMDD
-if [[ ! "$oldEdition" =~ routing([0-9]{6}) ]]; then
-  echo "#	The supplied argument must specify a routing edition of the form routingYYMMDD, but this was received: ${oldEdition}."
-  exit 1
-fi
-
-# Extract the date part of the routing database
-importDate=${BASH_REMATCH[1]}
-
 # Drop the routing and planet databases
-${superMysql} cyclestreets -e "drop database if exists ${oldEdition};";
-${superMysql} cyclestreets -e "drop database if exists planetExtractOSM${importDate};";
+${superMysql} cyclestreets -e "drop database if exists ${removeEdition};";
+${superMysql} cyclestreets -e "drop database if exists planetExtractOSM${editionDate};";
 
 # Remove the routing folder without generating any prompts or warnings
 if [ -n "${websitesContentFolder}" -a -d ${websitesContentFolder}/data/routing/ ]; then
-    rm -rf ${websitesContentFolder}/data/routing/${oldEdition}
+    rm -rf ${websitesContentFolder}/data/routing/${removeEdition}
 fi
 
 # Remove from the import output (may only be a symlink from there)
 if [ -n "${importContentFolder}" -a -d ${importContentFolder}/output/ ]; then
-    rm -rf ${importContentFolder}/output/${oldEdition}
+    rm -rf ${importContentFolder}/output/${removeEdition}
 fi
 
 # Report
-vecho "#	$(date)	Removed: $oldEdition"
+vecho "#	$(date)	Removed: ${removeEdition}"
 
 # Remove the lock file - ${0##*/} extracts the script's basename
 ) 9>$lockdir/${0##*/}

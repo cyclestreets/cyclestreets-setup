58e49ac33dbe430ae91f8371285e093f272c5a42
mvl22 <martin@lucas-smith.co.uk>
2016-07-01 11:52:20 +0100

    Load balancer: Copy in skeleton script

---
+++ install-loadbalancer/run.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# Installs the load balancer
+
+### Stage 1 - general setup
+
+echo "#	CycleStreets: install load balancer"
+
+# Ensure this script is run as root
+if [ "$(id -u)" != "0" ]; then
+    echo "#     This script must be run as root." 1>&2
+    exit 1
+fi
+
+# Bomb out if something goes wrong
+set -e
+
+# Lock directory
+lockdir=/var/lock/cyclestreets
+mkdir -p $lockdir
+
+# Set a lock file; see: http://stackoverflow.com/questions/7057234/bash-flock-exit-if-cant-acquire-lock/7057385
+(
+	flock -n 9 || { echo '#	An installation is already running' ; exit 1; }
+
+
+### CREDENTIALS ###
+
+# Get the script directory see: http://stackoverflow.com/a/246128/180733
+# The multi-line method of geting the script directory is needed to enable the script to be called from elsewhere.
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SCRIPTDIRECTORY=$DIR
+
+# Define the location of the credentials file relative to script directory
+configFile=../.config.sh
+
+# Generate your own credentials file by copying from .config.sh.template
+if [ ! -x $SCRIPTDIRECTORY/${configFile} ]; then
+    echo "#	The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file." 1>&2
+    exit 1
+fi
+
+# Load the credentials
+. $SCRIPTDIRECTORY/${configFile}
+
+# Announce starting
+echo "# Load balancer installation $(date)"
+
+
+## Main body
+
+# Shortcut for running commands as the cyclestreets user
+asCS="sudo -u ${username}"
+
+
+
+
+
+# Report completion
+echo "#	Installing load balancer completed"
+
+# Remove the lock file - ${0##*/} extracts the script's basename
+) 9>$lockdir/${0##*/}
+
+# End of file

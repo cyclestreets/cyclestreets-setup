f26e92615aef0ef8069acb98de04b516dac4d29c
si-the-pie <simon.nuttall@gmail.com>
2020-04-03 14:33:05 +0100

    Backup run script tidy

--- install-backup/run.sh
+++ install-backup/run.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 # Installs the backup system
+## This script is idempotent - it can be safely re-run without destroying existing data
 
-### Stage 1 - general setup
+## General setup
 
-echo "#	CycleStreets: install backup system"
+echo "#	CycleStreets: install/update backup system"
 
-# Ensure this script is run as root
+### Ensure this script is run as root
 if [ "$(id -u)" != "0" ]; then
     echo "#     This script must be run as root." 1>&2
     exit 1
 fi
 
-# Bomb out if something goes wrong
+### Bomb out if something goes wrong
 set -e
 
-# Lock directory
+### Lock directory
 lockdir=/var/lock/cyclestreets_outer
 mkdir -p $lockdir
 
-# Set a lock file; see: http://stackoverflow.com/questions/7057234/bash-flock-exit-if-cant-acquire-lock/7057385
+### Set a lock file; see: http://stackoverflow.com/questions/7057234/bash-flock-exit-if-cant-acquire-lock/7057385
 (
 	flock -n 900 || { echo '#	An installation is already running' ; exit 1; }
 
 
-### CREDENTIALS ###
+## CREDENTIALS
 
-# Get the script directory see: http://stackoverflow.com/a/246128/180733
-# The multi-line method of geting the script directory is needed to enable the script to be called from elsewhere.
+### Get the script directory see: http://stackoverflow.com/a/246128/180733
+### The multi-line method of geting the script directory is needed to enable the script to be called from elsewhere.
 SOURCE="${BASH_SOURCE[0]}"
 DIR="$( dirname "$SOURCE" )"
 while [ -h "$SOURCE" ]
@@ -38,77 +39,87 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 SCRIPTDIRECTORY=$DIR
 
-# Use this to remove the ../ to get the repository root; assumes the script is always down one level
+### Use this to remove the ../ to get the repository root; assumes the script is always down one level
 ScriptHome=$(readlink -f "${SCRIPTDIRECTORY}/..")
 
-# Define the location of the credentials file relative to script directory
+### Define the location of the credentials file relative to script directory
 configFile=$ScriptHome/.config.sh
 
-# Generate your own credentials file by copying from .config.sh.template
+### Generate your own credentials file by copying from .config.sh.template
 if [ ! -x $configFile ]; then
     echo "#	The config file, ${configFile}, does not exist or is not excutable - copy your own based on the ${configFile}.template file." 1>&2
     exit 1
 fi
 
-# Load the credentials
+### Load the credentials
 . $configFile
 
-# Shortcut for running commands as the cyclestreets user
+
+## Main body
+
+### Announce starting
+echo "# Backup system installation $(date)"
+
+## System Update
+
+### Shortcut for running commands as the cyclestreets user
 asCS="sudo -u ${username}"
 
-# Installer
+### Installer
 [[ $baseOS = "Ubuntu" ]] && packageInstall="apt -y install" || packageInstall="brew install"
 [[ $baseOS = "Ubuntu" ]] && packageUpdate="apt update" || packageUpdate="brew update"
 
-# Prepare the apt index; it may be practically non-existent on a fresh VM
+### Prepare the apt index; it may be practically non-existent on a fresh VM
 $packageUpdate > /dev/null
 
-# Bring the machine distribution up to date by updating all existing packages
+### Bring the machine distribution up to date by updating all existing packages
 apt -y upgrade
 apt -y dist-upgrade
 apt -y autoremove
 $packageInstall update-manager-core
 
-# Ensure locale
+### Ensure locale
 $packageInstall language-pack-en-base
 
-# Announce starting
-echo "# Backup system installation $(date)"
 
-## Main body
+## Email / exim
 
-# Setup directory structure:
-mkdir -p /websites/cyclescape/backups/rotation/
-mkdir -p /websites/www/archive/
-mkdir -p /websites/www/backups/rotation/
-mkdir -p /websites/www/data/
-
-# Make them writable
-chown ${username}.${rollout} -R /websites
-
-
-# Add Exim, so that mail will be sent, and add its configuration, but firstly backing up the original exim distribution config file if not already done
+### Add Exim, so that mail will be sent, and add its configuration, but firstly backing up the original exim distribution config file if not already done
 if $configureExim ; then
-    # NB The config here is currently Debian/Ubuntu-specific
+    ### NB The config here is currently Debian/Ubuntu-specific
     $packageInstall exim4
     if [ ! -e /etc/exim4/update-exim4.conf.conf.original ]; then
 	cp -pr /etc/exim4/update-exim4.conf.conf /etc/exim4/update-exim4.conf.conf.original
     fi
-    # NB These will deliberately overwrite any existing config; it is assumed that once set, the config will only be changed via this setup script (as otherwise it is painful during testing)
+    ### NB These will deliberately overwrite any existing config; it is assumed that once set, the config will only be changed via this setup script (as otherwise it is painful during testing)
     sed -i "s/dc_eximconfig_configtype=.*/dc_eximconfig_configtype='${dc_eximconfig_configtype}'/" /etc/exim4/update-exim4.conf.conf
     sed -i "s/dc_local_interfaces=.*/dc_local_interfaces='${dc_local_interfaces}'/" /etc/exim4/update-exim4.conf.conf
     sed -i "s/dc_readhost=.*/dc_readhost='${dc_readhost}'/" /etc/exim4/update-exim4.conf.conf
     sed -i "s/dc_smarthost=.*/dc_smarthost='${dc_smarthost}'/" /etc/exim4/update-exim4.conf.conf
-    # NB These two are the same in any CycleStreets installation but different from the default Debian installation:
+    ### NB These two are the same in any CycleStreets installation but different from the default Debian installation:
     sed -i "s/dc_other_hostnames=.*/dc_other_hostnames=''/" /etc/exim4/update-exim4.conf.conf
     sed -i "s/dc_hide_mailname=.*/dc_hide_mailname='true'/" /etc/exim4/update-exim4.conf.conf
     sudo systemctl restart exim4
 fi
 
-# Report completion
+
+## Backup system
+
+### Establish directory structure
+mkdir -p /websites/cyclescape/backups/rotation/
+mkdir -p /websites/www/archive/
+mkdir -p /websites/www/backups/rotation/
+mkdir -p /websites/www/data/
+
+### Make them writable
+chown ${username}.${rollout} -R /websites
+
+
+
+### Report completion
 echo "#	Installing backup system completed"
 
-# Remove the lock file - ${0##*/} extracts the script's basename
+## Remove the lock file - ${0##*/} extracts the script's basename
 ) 900>$lockdir/${0##*/}
 
 # End of file

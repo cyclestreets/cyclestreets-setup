a5f29f445790c31b7bda695e2fd37a048c917cfc
si-the-pie <simon.nuttall@gmail.com>
2021-01-12 21:56:07 +0000

    Install website - adding api alias for localhost Moved from the FQDN line. Cloud init has an option manage_etc_hosts which controls the /etc/hosts file so the new location might be more stable.

--- install-website/run.sh
+++ install-website/run.sh
@@ -345,12 +345,12 @@ then
 	fi
     fi
 
+
     # Beware that the on the multipass setup the hostname appears twice on the 127.0.1.1 line for some unknown reason.
     # That is not caused by these scripts and does not seem to be harfmul.
-
-    # Add aliases to the line that is often used for the FQDN of the machine
+    echo "#	Adding aliases for localhost: ${aliases}"
     sed -i \
-	-e "s/^127.0.1.1.*$/\0 ${aliases} # Alias added by CycleStreets installation/" \
+	-e "s/^127.0.0.1.*$/\0 ${aliases} # Alias added by CycleStreets installation/" \
 	/etc/hosts
 fi
 

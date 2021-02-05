2ca612c274fb6538656bd97aeacade80492e7a58
si-the-pie <simon.nuttall@gmail.com>
2021-02-05 14:08:44 +0000

    Cronned cyclestreets-update applied with every deploy.sh - not just first time

--- utility/installCommon.sh
+++ utility/installCommon.sh
@@ -18,16 +18,16 @@ if [ ! -d ${websitesContentFolder}/.git ]
 then
     su - ${SUDO_USER} -c "git clone ${repoOrigin}cyclestreets/cyclestreets.git ${websitesContentFolder}"
 
-    # Add cronned update of the repo
-    cp /opt/cyclestreets-setup/live-deployment/cyclestreets-update.cron /etc/cron.d/cyclestreets-update
-    chown root.root /etc/cron.d/cyclestreets-update
-    chmod 0600 /etc/cron.d/cyclestreets-update
-
 else
     # Set permissions before the update
     chgrp -R rollout ${websitesContentFolder}/.git
     su - ${SUDO_USER} -c "cd ${websitesContentFolder} && git pull"
 fi
+
+# Add cronned update of the repo
+cp /opt/cyclestreets-setup/live-deployment/cyclestreets-update.cron /etc/cron.d/cyclestreets-update
+chown root.root /etc/cron.d/cyclestreets-update
+chmod 0600 /etc/cron.d/cyclestreets-update
 
 # Ensure there's a custom sudoers file
 csSudoers=/etc/sudoers.d/cyclestreets

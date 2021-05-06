ea1af785c95b9e022b4bf8c4808ecab32b3a0d35
si-the-pie <simon.nuttall@gmail.com>
2021-05-06 16:06:10 +0100

    Install website configure testsAdditional

--- install-website/run.sh
+++ install-website/run.sh
@@ -527,6 +527,7 @@ then
 	-e "s/YOUR_CSSERVERNAME/${csHostname}/g" \
 	-e "s/YOUR_APISERVERNAME/${apiHostname}/g" \
 	-e "s/YOUR_HOSTPORT/${hostPortwithColon}/g" \
+	-e "s/TESTS_ADDITIONAL_HERE/${testsAdditional}/g" \
 	-e "s/#YOUR_ROUTINGENGINETIMEOUT/${retoReplacement}/" \
 	${phpConfig}
 

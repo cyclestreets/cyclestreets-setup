db1017ef07b572e16f12a965e5f1c3faef5849ec
mvl22 <martin@lucas-smith.co.uk>
2022-04-18 17:21:34 +0100

    Update git rollout following release of Git 2.35.2, for main CycleStreets repo
    See: https://github.blog/2022-04-12-git-security-vulnerability-announced/

--- utility/installCommon.sh
+++ utility/installCommon.sh
@@ -16,8 +16,9 @@ chown ${SUDO_USER} ${websitesContentFolder}
 # !! This may prompt for git username / password.
 if [ ! -d ${websitesContentFolder}/.git ]
 then
-    su - ${SUDO_USER} -c "git clone ${repoOrigin}cyclestreets/cyclestreets.git ${websitesContentFolder}"
-
+	su - ${SUDO_USER} -c "git clone ${repoOrigin}cyclestreets/cyclestreets.git ${websitesContentFolder}"
+	git config --global --add safe.directory ${websitesContentFolder}
+	
 else
     # Set permissions before the update
     chgrp -R rollout ${websitesContentFolder}/.git

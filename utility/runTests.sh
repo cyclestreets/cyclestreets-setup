d1a91e085411c67b3ac9c503d03762ed5fca7c8f
si-the-pie <simon.nuttall@gmail.com>
2022-12-30 14:53:45 +0000

    Utility script to run tests

---
+++ utility/runTests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Description
+#	Utility to run CycleStreets tests
+# Synopsis
+#	Expects summaryFile variable to be setup already.
+
+## Switch to main folder
+if [ -z "${websitesContentFolder}" ]; then
+    websitesContentFolder=/websites/www/content
+fi
+cd "${websitesContentFolder}"
+
+# Generate Tests Summary message
+if [ -z "${summaryFile}" ]; then
+    summaryFile=testResults.txt
+fi
+echo -e "#\tTests Summary" > ${summaryFile}
+
+# Run tests relevant to the new build, appending to summary
+echo -e "#\t$(date) Starting nearest point tests" >> ${summaryFile}
+php runtests.php "call=nearestpoint" >> ${summaryFile}
+
+echo -e "#\t$(date) Starting journey API 1 tests" >> ${summaryFile}
+php runtests.php "call=journey&apiVersion=1" >> ${summaryFile}
+
+echo -e "#\t$(date) Starting journey API 2 tests" >> ${summaryFile}
+php runtests.php "call=journey&apiVersion=2" >> ${summaryFile}
+
+# Compare new coverage with when the elevation.values auto tests were created
+echo -e "#\t$(date) Starting elevation auto generated tests" >> ${summaryFile}
+php runtests.php "call=elevation.values&name=Elevation auto generated test:" >> ${summaryFile}
+
+# Finished
+echo -e "#\t$(date) Completed tests" >> ${summaryFile}
+
+# End of file

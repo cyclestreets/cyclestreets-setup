0018d32feba4983c066e247ab79e3e2a39517bfd
mvl22 <martin@lucas-smith.co.uk>
2023-12-18 00:04:26 +0000

    Update node.js to v20 and use newer installation method

--- utility/installBaseWebserver.sh
+++ utility/installBaseWebserver.sh
@@ -265,9 +265,14 @@ if [ -f /etc/apparmor.d/usr.sbin.mysqld -a ! -f /etc/apparmor.d/disable/usr.sbin
 	set -e
 fi
 
-# Node - later version than v. 8.10.0 which is supplied with Ubuntu 18.04
-apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
+# Node.js; see: https://github.com/nodesource/distributions#installation-instructions
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
 apt-get install -y nodejs
 
 # Yarn, for JS package management; see: https://www.howtoforge.com/how-to-install-yarn-npm-package-manager-on-ubuntu-20-04/

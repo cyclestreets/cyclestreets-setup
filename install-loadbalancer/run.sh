fe379cf64c08763e8f45694f5ac8ae819b2280bf
mvl22 <martin@lucas-smith.co.uk>
2024-12-10 21:16:41 +0000

    Adjust path

--- install-loadbalancer/run.sh
+++ install-loadbalancer/run.sh
@@ -88,7 +88,7 @@ munin-node-configure --suggest
 set -e
 
 # Copy load balancer server config
-cp -pr $SCRIPTDIRECTORY/install-loadbalancer/cyclestreets-redirection-load-balancer.conf /etc/apache2/conf-enabled/cyclestreets-redirection-load-balancer.conf
+cp -pr $SCRIPTDIRECTORY/cyclestreets-redirection-load-balancer.conf /etc/apache2/conf-enabled/cyclestreets-redirection-load-balancer.conf
 service apache2 restart
 
 

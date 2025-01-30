52d0370c2adec6d092468a44e3a52eb941b48b70
mvl22 <martin@lucas-smith.co.uk>
2025-01-30 22:48:36 +0000

    Install base webserver: Fix idempotency for node key

--- utility/installBaseWebserver.sh
+++ utility/installBaseWebserver.sh
@@ -269,6 +269,7 @@ fi
 apt-get update
 apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
+rm -f /etc/apt/keyrings/nodesource.gpg		# In case already exists
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list

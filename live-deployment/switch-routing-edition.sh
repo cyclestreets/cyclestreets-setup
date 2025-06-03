cec07c9b3ed6283df734b88a778c698d313fa81f
si-the-pie <simon.nuttall@gmail.com>
2025-06-03 11:21:32 +0100

    Switch routing edtion: inline edition condition

--- live-deployment/switch-routing-edition.sh
+++ live-deployment/switch-routing-edition.sh
@@ -158,8 +158,7 @@ if [ "${multipleEditions}" = 1 ]; then
     fi
 
     # How is the alias currently being served
-    oldEditionCondition="from map_edition where alias = '${newEditionAlias}' and active = 'yes' limit 1;"
-    oldEditionDb=$(${superMysql} -s cyclestreets<<<"select routingDb ${oldEditionCondition}")
+    oldEditionDb=$(${superMysql} -s cyclestreets<<<"select routingDb from map_edition where alias = '${newEditionAlias}' and active = 'yes' limit 1;")
     oldEditionOrdering=$(${superMysql} -s cyclestreets<<<"select getEditionPriority('${oldEditionDb}');")
     oldEditionPort=$(${superMysql} -s cyclestreets<<<"select routingDb2port('${oldEditionDb}');")
     editionPort=${oldEditionPort}

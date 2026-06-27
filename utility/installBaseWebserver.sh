0ea19d476147bed13938db3c0cd0ccd5dcee292c
mvl22 <martin@lucas-smith.co.uk>
2026-06-27 20:30:37 +0100

    Update node to 24.x
    Whitelabel now assumes this

--- utility/installBaseWebserver.sh
+++ utility/installBaseWebserver.sh
@@ -293,15 +293,10 @@ if [ -f /etc/apparmor.d/usr.sbin.mysqld -a ! -f /etc/apparmor.d/disable/usr.sbin
 fi
 
 # Node.js; see: https://github.com/nodesource/distributions#installation-instructions
-apt-get update
-apt-get install -y ca-certificates curl gnupg
-mkdir -p /etc/apt/keyrings
-rm -f /etc/apt/keyrings/nodesource.gpg		# In case already exists
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-apt-get update
+apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 apt-get install -y nodejs
+node -v
 
 # Yarn, for JS package management; see: https://www.howtoforge.com/how-to-install-yarn-npm-package-manager-on-ubuntu-20-04/ and https://stackoverflow.com/a/74123377/
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarn.gpg >/dev/null
